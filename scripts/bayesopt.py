79bb4f66a1fc8d80661a13a784c7011ddff938d4
aterenin <aterenin@users.noreply.github.com>
2024-01-11 14:44:16 -0500

    Add wandb sweep.

---
+++ scripts/bayesopt.py
@@ -0,0 +1,14 @@
+import wandb
+
+def run_bayesopt_experiment(config):
+    print(config)
+    regret = config['dim']
+    time = config['length_scale']
+    return (regret,time)
+
+wandb.init()
+
+(regret,time) = run_bayesopt_experiment(wandb.config)
+
+wandb.log({"regret": regret})
+wandb.log({"time": time})
