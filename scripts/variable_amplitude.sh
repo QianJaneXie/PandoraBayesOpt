6fcacd53a75eb7f9da9931baa34367fc0c5d948f
Qian Xie <90579251+JaneQianXie@users.noreply.github.com>
2024-01-29 15:43:21 -0500

    update variable_amplitude

--- scripts/variable_amplitude.sh
+++ scripts/variable_amplitude.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=qx66@cornell.edu         # Email address to send results to
 #SBATCH -N 1                                 # Total number of nodes requested
 #SBATCH -n 1                                 # Total number of cores requested
-#SBATCH --array=0-300                        # Number of jobs
+#SBATCH --array=0-800                        # Number of jobs
 #SBATCH --mem-per-cpu=1000M                  # Server memory requested (per node)
 #SBATCH -t 2:00:00                           # Time limit (hh:mm:ss)
 #SBATCH --partition=default_partition        # Request partition
@@ -15,5 +15,5 @@
 source /share/apps/anaconda3/2021.05/etc/profile.d/conda.sh
 conda activate pandorabayesopt_env
 wandb login
-wandb agent 'ziv-scully-group/Gittins for Bayesian Optimization/i6ozs6w1' --count 1
+wandb agent 'ziv-scully-group/Gittins for Bayesian Optimization/y02899ru' --count 1
 conda deactivate
