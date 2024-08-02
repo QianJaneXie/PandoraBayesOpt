Correct the bool variable name for normalization
Change "input_normalization" to "output_normalization" and "input_standardize" to "output_standardize" as they should refer to the normalization of the y values while I initially thought the data (x, y) should be inputs to the GP surrogate model.

scripts/impact_of_lmbda.py
@@ -89,7 +89,7 @@ def run_bayesopt_experiment(config):
     if draw_initial_method == 'sobol':
         bounds = torch.stack([torch.zeros(dim), torch.ones(dim)])
         init_x = draw_sobol_samples(bounds=bounds, n=1, q=2*(dim+1)).squeeze(0)
-    input_standardize = config['input_normalization']
+    output_standardize = config['output_normalization']
 
     policy = config['policy']
     print("policy:", policy)
@@ -100,7 +100,7 @@ def run_bayesopt_experiment(config):
         initial_points=init_x,
         objective=objective_function, 
         kernel=scale_kernel,
-        input_standardize=input_standardize
+        output_standardize=output_standardize
     )
     if policy == 'Gittins_Lambda_1':
         Optimizer.run(

scripts/synthetic.py
@@ -28,7 +28,7 @@ def run_bayesopt_experiment(config):
     num_iterations = 10*dim
     seed = config['seed']
     torch.manual_seed(seed)
-    input_standardize = config['input_normalization']
+    output_standardize = config['output_normalization']
     draw_initial_method = config['draw_initial_method']
     policy = config['policy']
     print("policy:", policy)
@@ -63,7 +63,7 @@ def run_bayesopt_experiment(config):
         maximize=maximize, 
         initial_points=init_x,
         objective=objective_function,
-        input_standardize=input_standardize
+        output_standardize=output_standardize
     )
     if policy == 'RandomSearch':
         Optimizer.run(

scripts/synthetic_cost_aware.py
@@ -30,7 +30,7 @@ def run_bayesopt_experiment(config):
     dim = config['dim']
     seed = config['seed']
     torch.manual_seed(seed)
-    input_standardize = config['input_normalization']
+    output_standardize = config['output_normalization']
     draw_initial_method = config['draw_initial_method']
     policy = config['policy']
     budget = config['budget_to_dimension_ratio']*dim
@@ -133,7 +133,7 @@ def run_bayesopt_experiment(config):
         initial_points=init_x,
         objective=objective_function, 
         cost=cost_function,
-        input_standardize=input_standardize
+        output_standardize=output_standardize
     )
     if policy == 'RandomSearch':
         Optimizer.run_until_budget(

scripts/variable_amplitude.py
@@ -46,7 +46,7 @@ def run_bayesopt_experiment(config):
 
     seed = config['seed']
     torch.manual_seed(seed)
-    input_standardize = config['input_normalization']
+    output_standardize = config['output_normalization']
     policy = config['policy']
     print("policy:", policy)
     maximize = True
@@ -131,7 +131,7 @@ def run_bayesopt_experiment(config):
         initial_points=init_x, 
         kernel=kernel, 
         cost=cost_function,
-        input_standardize=input_standardize
+        output_standardize=output_standardize
     )
     if policy == 'ExpectedImprovementWithoutCost':
         Optimizer.run_until_budget(