50a804ea163f2e06c23355b03d0ac204b1e26b9c
Qian Xie <90579251+JaneQianXie@users.noreply.github.com>
2024-09-23 00:45:12 -0400

    add lcb acquisition function

---
+++ pandora_bayesopt/acquisition/lcb.py
@@ -0,0 +1,63 @@
+from typing import Dict, Optional, Union
+
+import torch
+from torch import Tensor
+from botorch.acquisition import AnalyticAcquisitionFunction
+from botorch.models.model import Model
+from botorch.acquisition.objective import PosteriorTransform
+from botorch.utils.transforms import t_batch_mode_transform
+
+
+class LowerConfidenceBound(AnalyticAcquisitionFunction):
+    r"""Single-outcome Lower Confidence Bound (LCB).
+
+    Analytic lower confidence bound that comprises of the posterior mean plus an
+    additional term: the posterior standard deviation weighted by a trade-off
+    parameter, `beta`. Only supports the case of `q=1` (i.e. greedy, non-batch
+    selection of design points). The model must be single-outcome.
+
+    `LCB(x) = mu(x) - sqrt(beta) * sigma(x)`, where `mu` and `sigma` are the
+    posterior mean and standard deviation, respectively.
+
+    Example:
+        >>> model = SingleTaskGP(train_X, train_Y)
+        >>> LCB = LowerConfidenceBound(model, beta=0.2)
+        >>> lcb = LCB(test_X)
+    """
+
+    def __init__(
+        self,
+        model: Model,
+        beta: Union[float, Tensor],
+        posterior_transform: Optional[PosteriorTransform] = None,
+        maximize: bool = True,
+    ) -> None:
+        r"""Single-outcome Lower Confidence Bound.
+
+        Args:
+            model: A fitted single-outcome GP model (must be in batch mode if
+                candidate sets X will be)
+            beta: Either a scalar or a one-dim tensor with `b` elements (batch mode)
+                representing the trade-off parameter between mean and covariance
+            posterior_transform: A PosteriorTransform. If using a multi-output model,
+                a PosteriorTransform that transforms the multi-output posterior into a
+                single-output posterior is required.
+            maximize: If True, consider the problem a maximization problem.
+        """
+        super().__init__(model=model, posterior_transform=posterior_transform)
+        self.register_buffer("beta", torch.as_tensor(beta))
+        self.maximize = maximize
+
+    @t_batch_mode_transform(expected_q=1)
+    def forward(self, X: Tensor) -> Tensor:
+        r"""Evaluate the Upper Confidence Bound on the candidate set X.
+
+        Args:
+            X: A `(b1 x ... bk) x 1 x d`-dim batched tensor of `d`-dim design points.
+
+        Returns:
+            A `(b1 x ... bk)`-dim tensor of Upper Confidence Bound values at the
+            given design points `X`.
+        """
+        mean, sigma = self._mean_and_sigma(X)
+        return (mean if self.maximize else -mean) - self.beta.sqrt() * sigma
